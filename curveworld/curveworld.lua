b3afe1627ad9bf9ba5252a8ce995e35c0eeb8ff4
rgrams <ross.grams@gmail.com>
2023-06-24 10:08:58 -0400

    Add more functions to control culling, make auto-cull-frustum script
    - Allow disabling frustum culling.
    - Allow only setting the culling projection, while using the normal
      view matrix.

--- curveworld/curveworld.lua
+++ curveworld/curveworld.lua
@@ -1,10 +1,14 @@
 
 local M = {}
 
+local persp_proj, ortho_proj = vmath.matrix4_perspective, vmath.matrix4_orthographic
+
 local origin = vmath.vector4()
 local curve = vmath.vector4()
 local constants -- Buffer - needs to be initialized with render.constant_buffer()
 local culling_frustum
+local culling_proj
+local culling_is_enabled = true
 
 function M.get_origin()
 	local cp = origin
@@ -48,18 +52,36 @@ function M.get_curve_offset(dx, dz)
 	local oy = -kz*curve.z - kx*curve.x
 	return ox, oy
 end
+local get_curve_offset = M.get_curve_offset
+
+function M.get_cull_extents(hw, hh, far)
+	local max_dist = math.sqrt(hw*hw + hh*hh + far*far)
+	local max_dx, max_dy = get_curve_offset(max_dist, max_dist)
+	local new_hw, new_hh = hw + math.abs(max_dx), hh + math.abs(max_dy)
+	return new_hw, new_hh
+end
+local get_cull_extents = M.get_cull_extents
 
 local MAX_FOV = math.pi - 0.00000001
 
 function M.get_cull_fov(fov, aspect, far)
 	local hh = math.tan(fov/2) * far
 	local hw = hh * aspect -- aspect = w/h
-	local max_dist = math.sqrt(hw*hw + hh*hh + far*far)
-	local max_dx, max_dy = M.get_curve_offset(max_dist, max_dist)
-	local new_hw, new_hh = hw + math.abs(max_dx), hh + math.abs(max_dy)
+	local new_hw, new_hh = get_cull_extents(hw, hh, far)
 	new_hh = math.max(new_hh, new_hw/aspect)
 	local new_fov = math.min(math.atan(new_hh/far) * 2, MAX_FOV)
 	return new_fov
+end
+local get_cull_fov = M.get_cull_fov
+
+function M.get_persp_cull_proj(fov, aspect, near, far)
+	fov = get_cull_fov(fov, aspect, far)
+	return persp_proj(fov, aspect, near, far)
+end
+
+function M.get_ortho_cull_proj(hw, hh, near, far)
+	hw, hh = get_cull_extents(hw, hh, far)
+	return ortho_proj(-hw, hw, -hh, hh, near, far)
 end
 
 function M.render_init(self)
@@ -72,15 +94,25 @@ function M.set_cull_frustum(frustum)
 	culling_frustum = frustum
 end
 
-function M.calc_cull_frustum(self)
-	local win_width = render.get_window_width()
-	local win_height = render.get_window_height()
-	return vmath.matrix4_perspective(math.rad(135), win_width/win_height, 0.1, 1000) * self.view
+function M.set_cull_proj(proj)
+	culling_proj = proj
 end
 
-function M.get_draw_options(self)
-	local frustum = culling_frustum or M.calc_cull_frustum(self)
-	return { frustum = frustum, constants = constants }
+function M.set_culling_enabled(enabled)
+	culling_is_enabled = enabled
+end
+
+function M.get_culling_enabled()
+	return culling_is_enabled
+end
+
+function M.get_draw_options(self, render_proj)
+	local opts = { constants = constants }
+	if culling_is_enabled then
+		local frustum = culling_frustum or (culling_proj or render_proj) * self.view
+		opts.frustum = frustum
+	end
+	return opts
 end
 
 return M
